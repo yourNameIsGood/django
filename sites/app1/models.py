a3f849b842840dd03baf38ade7e110d45a027792
linzhen <linzhen@91waijiao.com>
2014-06-28 16:02:24 +0800

    class meta, -timestamp

--- sites/app1/models.py
+++ sites/app1/models.py
@@ -5,8 +5,13 @@ class BlogPost (models.Model ) :
     title = models.CharField(max_length=150)
     body = models.TextField()
     timestamp = models.DateTimeField()
+    class Meta:
+        ordering  = ('-timestamp',)
 
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = ( 'title', 'timestamp' )
+
+
+
 
 admin.site.register(BlogPost,BlogPostAdmin)
